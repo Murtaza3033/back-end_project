13f9ead6ceaaf2f962320009f9fd0a05e305ec41
Murtaza ali qazi <murtazahere303@gmail.com>
2024-07-02 17:12:00 +0500

    Utils code

--- src/index.js
+++ src/index.js
@@ -8,6 +8,19 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("Error",error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at PORT:${process.env.PORT}`);
+    })
+})
+
+.catch((err)=>{
+    console.log("Error connecting to MONGO DB",err);
+})
 
 
 
---
+++ src/utils/apiError.js
@@ -0,0 +1,24 @@
+class apiError extends Error{
+    constructor(
+        message="Something went Wrong",
+        statusCode,
+        stack="",
+        errors=[],
+        ){
+        super(message)
+        this.statusCode=statusCode
+        this.data=null
+        this.message=message
+        this.success=false
+        this.errors=errors
+
+        if (stack){
+            this.stack=stack
+        }  else{
+            Error.captureStackTrace(this, this.constructor)
+        } 
+
+}
+}
+
+export {apiError}
